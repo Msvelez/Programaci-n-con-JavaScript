d40a60d6fde038b1e764dc7308ce613265f03b9e
María Sofía <msvelez@unbosque.edu.co>
2026-04-09 13:51:45 +0000

    Añado constantes de riesgo

--- index.js
+++ index.js
@@ -16,13 +16,27 @@ let prestacionesLey = 0;
 const edad_minima = 18;
 const edad_maxima_beneficio = 25;
 const edad_jubilacion = 60;
+
+// Valores y Tarifas en Colombia para 2026
 const salario_minimo = 1750905;
 const salario_minimo_integral_vigente = 22761765;
 const subsidio_transporte = 249.095;
 const uvt = 52.37;
-const fondo_Solidaridad_Pensional = 0;
-const Retencion_en_la_fuente = 0;
-const salud = salario * 4%;
+
+// Ingreso de información salarial
+const comisiones = 0;
+const total_horas_extra = 0;
+const clasificacion_nivel_de_riesgo = 0;
+
+// Tarifas de ARL (IBC)
+const riesgo_uno_minimo = 0.522;
+const riesgo_dos_bajo = 1.044;
+const riesgo_tres_medio = 2.436;
+const riesgo_cuatro_alto = 4.350;
+const riesgo_cinco_maximo = 6.960;
+
+
+
 
 
 
