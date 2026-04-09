8c2a43e1e6c352288bf614e302bc5d2f5903b227
María Sofía <msvelez@unbosque.edu.co>
2026-04-09 13:49:16 +0000

    Declaración de variables y constantes

---
+++ index.js
@@ -0,0 +1,30 @@
+
+// Ingreso de información básica 
+let nombreCompleto = "";
+let edad = 0;
+let tipoDocumento = "";
+let numeroDocumento = "";
+
+// Variables de entrada
+let salario = 0;
+let mesadaPensional = 0;
+let clasificacion = ""; 
+let pagoPension = 0;
+let prestacionesLey = 0;
+
+// Valores fijos
+const edad_minima = 18;
+const edad_maxima_beneficio = 25;
+const edad_jubilacion = 60;
+const salario_minimo = 1750905;
+const salario_minimo_integral_vigente = 22761765;
+const subsidio_transporte = 249.095;
+const uvt = 52.37;
+const fondo_Solidaridad_Pensional = 0;
+const Retencion_en_la_fuente = 0;
+const salud = salario * 4%;
+
+
+
+
+
